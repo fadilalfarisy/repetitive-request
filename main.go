189f51188fb7979946531871ae14d6de26210cdd
Ahmad Fadil Alfarisy <ahmad.fadilalfarisy@gmail.com>
2023-04-05 12:16:39 +0700

    inital commit

--- main.go
+++ main.go
@@ -1,36 +1,59 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
-func main() {
-	http.HandleFunc("/", RandomPost)
-
-	server := new(http.Server)
-	server.Addr = ":3000"
-
-	fmt.Println("server started at localhost:3000")
-	server.ListenAndServe()
+type Target struct {
+	Wind  int
+	Water int
 }
 
-func RandomPost(water int, wind int) {
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	randomWater := rand.Intn(100)
+	randomWind := rand.Intn(100)
+
 	data := map[string]interface{}{
-		"water": water,
-		"wind":  wind,
+		"water": randomWater,
+		"wind":  randomWind,
 	}
+
+	var statusWater string
+	var statusWind string
+
+	if 5 >= randomWater {
+		statusWater = "aman"
+	} else if randomWater >= 6 && randomWater <= 8 {
+		statusWater = "siaga"
+	} else {
+		statusWater = "bahaya"
+	}
+
+	if 6 >= randomWind {
+		statusWind = "aman"
+	} else if randomWind >= 7 && randomWind <= 15 {
+		statusWind = "siaga"
+	} else {
+		statusWind = "bahaya"
+	}
+
 	requestJSON, err := json.Marshal(data)
 	client := &http.Client{}
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	req, err := http.NewRequest("POST", "https://jsonplaceholder.typicode.com/posts", requestJSON)
-	req.Header.Set("Content Type", "applicatoin/json")
+	req, err := http.NewRequest("POST", "https://jsonplaceholder.typicode.com/posts", bytes.NewBuffer(requestJSON))
+	req.Header.Set("Content-type", "application/json")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -47,5 +70,50 @@ func RandomPost(water int, wind int) {
 		log.Fatalln(err)
 	}
 
-	log.Println(string(body))
+	fmt.Println(string(body))
+	fmt.Printf("status water : %s \n", statusWater)
+	fmt.Printf("status wind : %s \n", statusWind)
 }
+
+// package main
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"net/http"
+// )
+
+// func main() {
+// 	http.HandleFunc("/", ActionRandom)
+
+// 	server := new(http.Server)
+// 	server.Addr = ":3000"
+
+// 	fmt.Println("server started at localhost:3000")
+// 	server.ListenAndServe()
+// }
+
+// func ActionRandom(w http.ResponseWriter, r *http.Request) {
+// 	if !AllowOnlyGET(w, r) {
+// 		return
+// 	}
+
+// 	if id := r.URL.Query().Get("userID"); id != "" {
+// 		w.Header().Set("Content-Type", "application/json")
+// 		w.Write(res)
+// 		return
+// 	}
+
+// 	OutputJSON(w, GetStudents())
+// }
+
+// func OutputJSON(w http.ResponseWriter, o interface{}) {
+// 	res, err := json.Marshal(o)
+// 	if err != nil {
+// 		w.Write(([]byte(err.Error())))
+// 		return
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.Write(res)
+// }
